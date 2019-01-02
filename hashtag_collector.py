3c64adbdfa7d77cac5d40f3b6f13cc5d75413b17
Mehdi Kian <mehdi.kianpour@sentia.com>
2019-01-02 18:49:58 +0100

    minor modifications

--- hashtag_collector.py
+++ hashtag_collector.py
@@ -16,7 +16,6 @@ def lambda_handler(event, context):
     auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
     # auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
-    #####United Airlines
     # Open/Create a file to append data
     csvFile = open('/tmp/aws_hashtag.csv', 'a')
     #Use csv Writer
@@ -24,9 +23,9 @@ def lambda_handler(event, context):
 
     hashtag_entry = f"#{hashtag}"
     s3 = boto3.resource('s3')
-    csvWriter.writerow(["created_at", "tweet_context"])
+
     for tweet in tweepy.Cursor(api.search,q=hashtag_entry,count=100,
-                               since="2018-12-31").items():
+                               since="2019-01-01").items():
         csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
     s3.Bucket(datalake_bucket).upload_file("/tmp/aws_hashtag.csv",
                                             f"twitter-data/hashtags/{hashtag}/aws_hashtag.csv")
