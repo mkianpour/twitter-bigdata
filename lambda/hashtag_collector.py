b3d9c368a75c5be6a6936ee694a826e11e2119c7
Mehdi Kian <mehdi.kianpour@sentia.com>
2019-01-25 16:21:24 +0100

    relational timing, lambda moved to a folder

--- lambda/hashtag_collector.py
+++ lambda/hashtag_collector.py
@@ -2,6 +2,7 @@ import boto3
 import tweepy
 import csv
 import os
+import datetime
 
 def lambda_handler(event, context):
     ####input your credentials here
@@ -11,21 +12,31 @@ def lambda_handler(event, context):
     access_token_secret = os.environ['ACCESS_SECRET']
 
     hashtag = os.environ['HASHTAG']
+    # hashtag = event["hashtag"]
     datalake_bucket = os.environ['DATALAKE_BUCKET']
 
     auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
     # auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
     # Open/Create a file to append data
-    csvFile = open('/tmp/aws_hashtag.csv', 'a')
+    csvFile = open('/tmp/hashtag.csv', 'a')
     #Use csv Writer
     csvWriter = csv.writer(csvFile)
 
     hashtag_entry = f"#{hashtag}"
     s3 = boto3.resource('s3')
 
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    y_day = yesterday.strftime("%Y-%m-%d")
+    print ("y_day: ", y_day, " yesterday: ", yesterday)
+
     for tweet in tweepy.Cursor(api.search,q=hashtag_entry,count=100,
-                               since="2019-01-01").items():
-        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-    s3.Bucket(datalake_bucket).upload_file("/tmp/aws_hashtag.csv",
-                                            f"twitter-data/hashtags/{hashtag}/aws_hashtag.csv")
+                               since=y_day).items():
+        csvWriter.writerow([hashtag,
+                            tweet.user.name,
+                            tweet.created_at,
+                            tweet.text.encode('utf-8')])
+    now = datetime.datetime.now()
+    now_dir = now.strftime("%Y-%m-%d")
+    s3.Bucket(datalake_bucket).upload_file("/tmp/hashtag.csv",
+                                            f"twitter-data/hashtags/{hashtag}/{now_dir}/hashtag.csv")
